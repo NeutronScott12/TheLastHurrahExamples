d182649a9ebdf035e567b7e9c4c1079aade5403b
scott <scottberry91@gmail.com>
2021-11-04 19:58:18 +0000

    Comments and Authentication working now

--- packages/the-last-hurrah-website-example/src/modules/blog/containers/BlogContainer.tsx
+++ packages/the-last-hurrah-website-example/src/modules/blog/containers/BlogContainer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-// import { BinaryStashCommentComponent } from '@thelasthurrah/binary-stash-comment-component'
+import { BinaryStashCommentComponent } from '@thelasthurrah/binary-stash-comment-component'
 
 import { blogs } from '../data/blogs'
 
@@ -12,9 +12,9 @@ export const BlogContainer = () => {
     const { slug } = useParams() as IParams
 
     const data = blogs.find((blog) => blog.slug === slug)
-    // const application_name = 'Second Application'
-    // const application_id = '63185b27-f626-40fa-a0ea-ae7eb33f0bdf'
-    // const website_url = window.location.href
+    const application_name = 'Second Application'
+    const application_id = '63185b27-f626-40fa-a0ea-ae7eb33f0bdf'
+    const website_url = window.location.href
 
     console.log(data)
     return data ? (
@@ -22,12 +22,12 @@ export const BlogContainer = () => {
             <h2>{data.title}</h2>
             <p>{data.content}</p>
 
-            {/* <BinaryStashCommentComponent
+            <BinaryStashCommentComponent
                 application_name={application_name}
                 application_id={application_id}
                 title={data.title}
                 website_url={website_url}
-            /> */}
+            />
         </div>
     ) : (
         <div>Loading...</div>
