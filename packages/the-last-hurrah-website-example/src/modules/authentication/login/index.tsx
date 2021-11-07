91c9cdeca033a3cf8c1472da2fa6d932705e0493
scott <scottberry91@gmail.com>
2021-11-07 20:58:48 +0000

    got theme working

--- packages/the-last-hurrah-website-example/src/modules/authentication/login/index.tsx
+++ packages/the-last-hurrah-website-example/src/modules/authentication/login/index.tsx
@@ -11,7 +11,7 @@ import { useNavigate } from 'react-router-dom'
 export const LoginContainer = () => {
     const navigate = useNavigate()
 
-    let application_id = '63185b27-f626-40fa-a0ea-ae7eb33f0bdf'
+    let application_id = '76d10d95-8d64-4f19-b213-af1eb78f0e9e'
     let application_name = 'Second Application'
 
     const dispatch = useAuthDispatch()
--- packages/the-last-hurrah-website-example/src/modules/blog/containers/BlogContainer.tsx
+++ packages/the-last-hurrah-website-example/src/modules/blog/containers/BlogContainer.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { BinaryStashCommentComponent } from '@thelasthurrah/binary-stash-comment-component'
 
 import { blogs } from '../data/blogs'
+import { useTheme } from '@mui/material'
 
 interface IParams {
     slug: string
@@ -10,19 +11,24 @@ interface IParams {
 
 export const BlogContainer = () => {
     const { slug } = useParams() as IParams
+    const theme = useTheme()
 
     const data = blogs.find((blog) => blog.slug === slug)
     const application_name = 'Second Application'
-    const application_id = '63185b27-f626-40fa-a0ea-ae7eb33f0bdf'
+    const application_id = '76d10d95-8d64-4f19-b213-af1eb78f0e9e'
     const website_url = window.location.href
 
-    console.log(data)
+    const dark_theme = theme.palette.mode === 'dark' ? true : false
+
+    console.log('DARK_THEME', dark_theme)
+
     return data ? (
         <div>
             <h2>{data.title}</h2>
             <p>{data.content}</p>
 
             <BinaryStashCommentComponent
+                dark_theme={dark_theme}
                 application_name={application_name}
                 application_id={application_id}
                 title={data.title}
